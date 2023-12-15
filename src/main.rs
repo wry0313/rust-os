ed762e5c97ee7365205f3b9a60af9085ab354394
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-12-15 13:32:07 -0800

    paging

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
 use rust_os::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     rust_os::init();
