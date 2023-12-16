44e2560bfdeab2fa5f8750144f7eb07e497954a3
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-12-16 15:19:53 -0800

    create mapping

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,28 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
+use x86_64::structures::paging::Page;
 use core::panic::PanicInfo;
-use bootloader::{BootInfo, entry_point};
 use rust_os::println;
 
 entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rust_os::memory::active_level_4_table;
     use x86_64::VirtAddr;
+    use rust_os::memory;
 
-    println!("Hello World{}", "!");
     rust_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
+    
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-        }
-    }
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
 
     #[cfg(test)]
     test_main();
